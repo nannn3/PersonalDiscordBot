bb889eb791c47557b74b74033eb5fc5e152f9dd1
Tristen <millert9999@gmail.com>
2022-06-20 09:39:02 -0700

    started making a discord bot

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import nextcord
+from nextcord import Interaction
+from nextcord.ext import commands
+
+
+intents=nextcord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix='!',intents=intents)
+
+@client.event
+async def on_ready():
+    print("The bot is now ready for use")
+
+testingServerID = 988481933841416273
+
+@nextcord.slash_command(name="hello",description="Hello World")
+async def hellocommand(interaction: Interaction)
+    await interaction.response.send_message("Hello Wolrd")
+
+client.run('OTg4NDc3NTAyOTI4MzM0OTU4.Go32uL.cW-SzPkctlD1_n7iKzsI3KQ3kLLPcAdlqH7gmk')
 
