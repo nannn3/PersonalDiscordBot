525059c87cebf54eab3a89e98f6a5ee8f4527961
Tristen <trkmille@ucsc.edu>
2022-06-29 18:58:59 -0700

    Done for today

--- main.py
+++ main.py
@@ -1,52 +1,54 @@
-from asyncio.constants import SSL_HANDSHAKE_TIMEOUT
-import nextcord
-from nextcord import Interaction
-from nextcord.ext import commands
 import os
-from os.path import join, dirname
+import discord
+from discord.ext import commands
 from dotenv import load_dotenv
-import pdb
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-token = os.environ.get("token")
-serverTest = os.environ.get("testingServerID")
-serverTest=int(serverTest)
-intents = nextcord.Intents(messages=True,message_content = True, guilds=True)
 
 
-bot = commands.Bot(command_prefix='$')
+load_dotenv('.env')
+TOKEN = os.environ.get('token')
+TestServer= os.environ.get('testingServerID')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.reply('Pong!')
-@bot.event
-async def on_ready():#When the bot boots up
-    print("The bot is now ready for use")
 
-@bot.slash_command(guild_ids=[serverTest]) #Basic test of slash commands
-async def hello(interaction: Interaction):
-    await interaction.response.send_message("Hello Wolrd!")
-"""
-it looks like discord doesn't let this happen. Fair enough.
-@bot.event
+intents=discord.Intents.default()
+intents.members=True
+
+client=commands.Bot(command_prefix="!",intents=intents)
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord!")
+
+@client.event
+async def on_member_join(member):
+    pass
+
+@client.event
 async def on_message(message):
-    print("Message Recieved")
-    if message.author == bot.user:
+    '''collection of replys to messages with no prefix'''
+    if message.author == client.user:
         return
-    print(message.content)
-    pdb.set_trace()
-"""
 
-#set up COG reading folder
-Initial_extensions=[]
-
-for fileName in os.listdir('./cogs'):
-    if fileName.endswith('.py'):
-        Initial_extensions.append("cogs."+fileName[:-3])
-if __name__=='__main__':
-    for extension in Initial_extensions:
-        bot.load_extension(extension)
-
-token=str(token)
-bot.run(token)
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+        
+    if message.content.startswith('cutie'):
+        embed = discord.Embed(title=None)
+        embed.set_image(url='https://c.tenor.com/iESegr2Kb6MAAAAd/narpy-cute.gif')
+        await message.channel.send(embed=embed)
+@client.command(pass_context=True)
+async def join(ctx):
+        print("Join recieved")
+        if(ctx.author.voice):
+            channel=ctx.message.author.voice.channel
+            await channel.connect()
+        else:
+            await ctx.send("You're not in a voice channel, silly.")
+            
+@client.command(pass_context=True)
+async def leave(ctx):
+    print("leave recieved")
+    if(ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("Bye!")
+    else:
+        await ctx.send("I already left")
+client.run(TOKEN)
