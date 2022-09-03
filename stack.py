0eb99718f2ad98e3cd953efaaa2c055783360cef
Tristen <trkmille@ucsc.edu>
2022-09-03 12:18:36 -0700

    added tests to stack.py and minor formatting fixes

--- stack.py
+++ stack.py
@@ -13,19 +13,42 @@ class Stack:
         self.__index = -1
 
     def isEmpty(self):
-        return True if self.size() == 0 else False
+        return self.size() == 0
 
     def push(self, item):
         self.stack.append(item)
         self.__index += 1
 
     def pop(self):
-        foo = self.stack.pop(self.__index)
-        self.__index -= 1
-        return foo
+        if not self.isEmpty():
+            foo = self.stack.pop(self.__index)
+            self.__index -= 1
+            return foo
+        else:
+            return None
 
     def peek(self):
         return None if self.isEmpty() else self.stack[self.__index]
 
     def size(self):
         return len(self.stack)
+
+
+def test():
+    s = Stack()
+    assert s.isEmpty()
+    for i in range(10):
+        s.push(i)
+    assert s.peek() == 9
+    foo = s.pop()
+    assert foo == 9
+    assert s.isEmpty() is False
+    while not s.isEmpty():
+        s.pop()
+    foo = s.pop()
+    assert foo is None
+
+
+if __name__ == '__main__':
+    # not a script, but is self testing
+    test()
